1e02be29d0897645aa0cfb645eb63f8026843576
Omer <omerfarukdasar@gmail.com>
2022-07-08 00:27:32 +0200

    setup3

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 # Three modules for swagger:
 from rest_framework import permissions
@@ -13,7 +13,7 @@ schema_view = get_schema_view(
         default_version="v1",
         description="Flight Reservation API project provides flight and reservation info",
         terms_of_service="#",
-        contact=openapi.Contact(email="rafe@clarusway.com"),  # Change e-mail on this line!
+        contact=openapi.Contact(email="omerfarukdasar@gmail.com"),  # Change e-mail on this line!
         license=openapi.License(name="BSD License"),
     ),
     public=True,
@@ -28,4 +28,5 @@ urlpatterns = [
     path("swagger(<format>\.json|\.yaml)", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
