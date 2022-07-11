07b577530d83e243fa558eb1a1abe91e542455aa
Omer <omerfarukdasar@gmail.com>
2022-07-11 22:05:26 +0200

    models created

--- flight/admin.py
+++ flight/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from flight.models import Flight, Passenger, Reservation
+
+admin.site.register(Flight)
+admin.site.register(Passenger)
+admin.site.register(Reservation)
