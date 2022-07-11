884f2a7de837d27f9059ff18936e5bb4aec742aa
Omer <omerfarukdasar@gmail.com>
2022-07-12 00:22:50 +0200

    create method

--- flight/serializers.py
+++ flight/serializers.py
@@ -34,3 +34,13 @@ class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = ("id", "flight", "flight_id", "user", "user_id", "passenger")
+
+    def create(self, validated_data):
+        passenger_data = validated_data.pop("passenger")
+        validated_data["user_id"] = self.context["request"].user.id
+        reservation = Reservation.objects.create(**validated_data)
+        for passenger in passenger_data:
+            pas = Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+        reservation.save()
+        return reservation
