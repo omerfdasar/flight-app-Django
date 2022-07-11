65124679796ddee988aacae09fd5fa577010f4c6
Omer <omerfarukdasar@gmail.com>
2022-07-11 22:41:48 +0200

    flight view done

--- flight/views.py
+++ flight/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .serializers import FlightSerializer
 from .models import Flight, Passenger, Reservation
 from rest_framework import viewsets
+from .permission import IsStafforReadOnly
 
 
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes = (IsStafforReadOnly,)
