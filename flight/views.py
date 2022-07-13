74da0a6cdbbf2e4247cb022b7056bd807f5d129d
Omer <omerfarukdasar@gmail.com>
2022-07-13 23:08:27 +0200

    permission to see

--- flight/views.py
+++ flight/views.py
@@ -14,3 +14,9 @@ class FlightView(viewsets.ModelViewSet):
 class ReservationView(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.is_staff():
+            return queryset
+        return queryset.filter(user=self.request.user)
